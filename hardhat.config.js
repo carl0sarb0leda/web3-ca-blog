18b6d3054547c25b2c424d05b32f3fd0c522fcc2
carl0sarb0leda <carlosaepn@gmail.com>
2022-10-30 17:46:51 +1300

    Add dotenv package + update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
+const dotenv = require("dotenv");
+dotenv.config({path: __dirname + '/.env'});
+const { NEXT_PUBLIC_INFURA_PROJECT_ID, NEXT_PUBLIC_METAMASK_API_PRIVATE_KEY } = process.env;
+console.log('PK', NEXT_PUBLIC_METAMASK_API_PRIVATE_KEY);
+console.log('ProjectID:', NEXT_PUBLIC_INFURA_PROJECT_ID);
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -6,14 +11,14 @@ module.exports = {
   networks: {
     hardhat: {
       chainId: 1337
-    }
-    // mumbai: {
-    //   url: "https://rpc-mumbai.matic.today",
-    //   accounts: [process.env.pk]
-    // },
-    // polygon: {
-    //   url: "https://polygon-rpc.com/",
-    //   accounts: [process.env.pk]
-    // }
+    },
+    polygon: {
+      url: `https://polygon-mainnet.infura.io/v3/${NEXT_PUBLIC_INFURA_PROJECT_ID}`,
+      accounts: [NEXT_PUBLIC_METAMASK_API_PRIVATE_KEY]
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.matic.today",
+      accounts: [NEXT_PUBLIC_METAMASK_API_PRIVATE_KEY]
+    },
   }
 };
