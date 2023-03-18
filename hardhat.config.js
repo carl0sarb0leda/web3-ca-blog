e913d55f0f9919fbb01eeee9124d1289881fef51
carl0sarb0leda <carlosaepn@gmail.com>
2023-03-18 17:43:40 +1300

    Update env vars logic and hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,23 +1,25 @@
 require("@nomiclabs/hardhat-waffle");
 const dotenv = require("dotenv");
-dotenv.config({path: __dirname + '/.env'});
-const { NEXT_PUBLIC_INFURA_PROJECT_ID, NEXT_PUBLIC_METAMASK_API_PRIVATE_KEY } = process.env;
-const pk = NEXT_PUBLIC_METAMASK_API_PRIVATE_KEY || process.env.metamask_private_key
+dotenv.config({ path: __dirname + "/.env" });
+const { NEXT_PUBLIC_INFURA_API_KEY, NEXT_PUBLIC_METAMASK_API_PRIVATE_KEY } =
+  process.env;
+const pk =
+  NEXT_PUBLIC_METAMASK_API_PRIVATE_KEY || process.env.metamask_private_key;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
   networks: {
     hardhat: {
-      chainId: 1337
+      chainId: 1337,
     },
     polygon: {
-      url: `https://polygon-mainnet.infura.io/v3/${NEXT_PUBLIC_INFURA_PROJECT_ID}`,
-      accounts: [pk]
+      url: `https://polygon-mainnet.infura.io/v3/${NEXT_PUBLIC_INFURA_API_KEY}`,
+      accounts: [pk],
     },
     mumbai: {
-      url: "https://rpc-mumbai.matic.today",
-      accounts: [pk]
+      url: "https://rpc-mumbai.maticvigil.com/",
+      accounts: [pk],
     },
-  }
+  },
 };
